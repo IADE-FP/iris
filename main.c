303b1db7395f1138cdb6ef7b1de8f2ac08558131
amgs <amgs>
2023-11-26 23:22:34 +0000

    Refactor read_records.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 typedef struct {
     double sepal_length;
@@ -11,31 +11,38 @@ typedef struct {
     char* species;
 } Record;
 
-int main() {
-    FILE *f = fopen("iris.csv", "r");
-    char *line = NULL;
+Record* read_records(const char* filename, int* num_records) {
+    FILE* f = fopen(filename, "r");
+    char* line = NULL;
     size_t len = 0;
-    getline(&line, &len, f); // cabeçalho
-    free(line); line = NULL;
+    getline(&line, &len, f);  // cabeçalho
+    free(line);
+    line = NULL;
     Record* records = NULL;
-    int num_records = 0;
-    while(getline(&line, &len, f) != -1) {
+    *num_records = 0;
+    while (getline(&line, &len, f) != -1) {
         printf("%s", line);
         line[strlen(line) - 1] = '\0';
-        records = realloc(records, (num_records + 1) * sizeof(Record));
-        records[num_records].sepal_length = atof(strtok(line, ","));
-        records[num_records].sepal_width = atof(strtok(NULL, ","));
-        records[num_records].petal_length = atof(strtok(NULL, ","));
-        records[num_records].petal_width = atof(strtok(NULL, ","));
+        records = realloc(records, (*num_records + 1) * sizeof(Record));
+        records[*num_records].sepal_length = atof(strtok(line, ","));
+        records[*num_records].sepal_width = atof(strtok(NULL, ","));
+        records[*num_records].petal_length = atof(strtok(NULL, ","));
+        records[*num_records].petal_width = atof(strtok(NULL, ","));
         char* species = strtok(NULL, ",");
-        records[num_records].species = malloc(sizeof(char) * (strlen(species) + 1));
-        strcpy(records[num_records].species, species);
-        num_records++;
+        records[*num_records].species = malloc(sizeof(char) * (strlen(species) + 1));
+        strcpy(records[*num_records].species, species);
+        (*num_records)++;
         free(line);
         line = NULL;
     }
-    if(line != NULL) free(line);
+    if (line != NULL) free(line);
     fclose(f);
+    return records;
+}
+
+int main() {
+    int num_records = 0;
+    Record* records = read_records("iris.csv", &num_records);
 
     for (int i = 0; i < num_records; i++) {
         printf("%lf %lf %lf %lf %s\n", records[i].sepal_length, records[i].sepal_width, records[i].petal_length, records[i].petal_width, records[i].species);
